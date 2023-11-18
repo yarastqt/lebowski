4d086d6d4e24f2731074298797305da79f8a75e9
yarastqt <yarastqt@gmail.com>
2023-11-18 16:49:16 +0400

    feat: improve create debt screen

--- src/screens/create-debt-screen/create-debt-screen.tsx
+++ src/screens/create-debt-screen/create-debt-screen.tsx
@@ -1,28 +1,29 @@
 import { useUnit } from 'effector-react'
 import { FC } from 'react'
-import { View } from 'react-native'
 
 import { BaseLayout } from '@app/layouts/base-layout'
 import { useForm } from '@app/shared/lib/effector-form'
-import { ActionButton, TextField } from '@app/shared/ui'
+import { ActionButton, Form, Text, TextField } from '@app/shared/ui'
 
 import { createDebtScreenModel } from './create-debt-screen-model'
 
 export const CreateDebtScreen: FC = () => {
-  const { fields, submit } = useForm(createDebtScreenModel.form)
+  const { fields, submit, isInvalid } = useForm(createDebtScreenModel.form)
   const { isPending } = useUnit(createDebtScreenModel)
 
   return (
-    <BaseLayout>
-      <View>
+    <BaseLayout edgets={{ top: 'off' }}>
+      <Form>
+        <Text variant="heading-m">Create new debt</Text>
+
         <TextField {...fields.reciverEmail.props} label="User email" />
         <TextField {...fields.amount.props} label="Amount" />
         <TextField {...fields.comment.props} label="Comment" />
 
-        <ActionButton onPress={submit} isPending={isPending}>
+        <ActionButton onPress={submit} isPending={isPending} isDisabled={isInvalid}>
           {isPending ? 'Creating...' : 'Create'}
         </ActionButton>
-      </View>
+      </Form>
     </BaseLayout>
   )
 }
