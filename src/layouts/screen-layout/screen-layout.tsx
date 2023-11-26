07112d922caed87c790603f1abf0fa0a8ce4c74d
yarastqt <yarastqt@gmail.com>
2023-11-26 19:37:50 +0400

    feat: pass edgets for screen layout

--- src/layouts/screen-layout/screen-layout.tsx
+++ src/layouts/screen-layout/screen-layout.tsx
@@ -1,15 +1,21 @@
 import { FC, ReactNode } from 'react'
+import { Edges } from 'react-native-safe-area-context'
 
 import { BaseLayout } from '../base-layout'
 import { Header } from './ui/header'
 
 export interface ScreenLayoutProps {
   children: ReactNode
+  edgets?: Edges
   title: string
 }
 
 export const ScreenLayout: FC<ScreenLayoutProps> = (props) => {
-  const { children, title } = props
+  const { children, edgets, title } = props
 
-  return <BaseLayout header={<Header>{title}</Header>}>{children}</BaseLayout>
+  return (
+    <BaseLayout edgets={edgets} header={<Header>{title}</Header>}>
+      {children}
+    </BaseLayout>
+  )
 }
