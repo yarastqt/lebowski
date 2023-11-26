2cae7f7a6cf8f9b8b63958db754d38134c0c46af
yarastqt <yarastqt@gmail.com>
2023-11-26 19:37:11 +0400

    feat: update status positive for dark theme

--- src/shared/theme/dark-theme.ts
+++ src/shared/theme/dark-theme.ts
@@ -23,7 +23,7 @@ export const darkTheme: Theme = {
     surfaceSubmerged: 'rgba(18, 18, 18, 1)',
     surfaceOverlay: 'rgba(14, 14, 15, 0.88)',
 
-    statusPositive: 'rgba(116, 214, 194, 1)',
+    statusPositive: 'rgba(165, 255, 213, 1)',
     statusNegative: 'rgba(251, 92, 86, 1)',
 
     textPrimary: 'rgba(255, 253, 250, 1)',
