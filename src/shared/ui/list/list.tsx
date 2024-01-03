6e2293294094ce1aaead686e555dcbdde430ef0f
yarastqt <yarastqt@gmail.com>
2024-01-03 18:07:17 +0300

    feat: impl default list style

--- src/shared/ui/list/list.tsx
+++ src/shared/ui/list/list.tsx
@@ -4,21 +4,29 @@ import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 import { createStyles } from '@app/shared/theme'
 
+export type ListStyle = 'default' | 'fill' | 'plain'
+
 export interface ListProps {
   children: ReactNode
-  listStyle?: 'fill' | 'plain'
+  listStyle?: ListStyle
 }
 
 export const List: FC<ListProps> = (props) => {
-  const { children, listStyle } = props
+  const { children, listStyle = 'default' } = props
 
   const styles = useStyles()
   const insets = useSafeAreaInsets()
 
+  const margins = {
+    default: 0,
+    fill: -12,
+    plain: -16,
+  } satisfies Record<ListStyle, number>
+
   return (
     <ScrollView
       showsVerticalScrollIndicator={false}
-      style={{ marginBottom: -insets.bottom, marginHorizontal: listStyle === 'fill' ? -12 : -16 }}
+      style={{ marginBottom: -insets.bottom, marginHorizontal: margins[listStyle] }}
     >
       <View style={styles.root}>{children}</View>
       <View style={{ height: insets.bottom }} />
